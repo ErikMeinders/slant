67e9425484045be14477202f16a8ad5551a7b3fd
Erik Meinders <erik@easytocloud.com>
2024-03-27 20:03:05 +0100

    Initial Commit

---
+++ slant/slantApp.swift
@@ -0,0 +1,32 @@
+//
+//  slantApp.swift
+//  slant
+//
+//  Created by Erik Meinders on 27/03/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct slantApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
